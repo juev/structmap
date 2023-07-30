bb6217c8adf83ec85df29475cf8885cea5974572
Evsyukov Denis <denis@evsyukov.org>
2023-07-30 15:02:49 +0300

    feat: change tests for StructToMap

--- func_test.go
+++ func_test.go
@@ -18,18 +18,41 @@ func TestStructToMap(t *testing.T) {
 			name: "simple convert",
 			args: args{
 				str: struct {
-					St string
-					In int16
-					Fl float32
-					Bo bool
+					St     string
+					In16   int16
+					In32   int32
+					In64   int64
+					Uint16 uint16
+					Uint32 uint32
+					Uint64 uint64
+					Fl32   float32
+					Fl64   float64
+					Bo     bool
 				}{
-					St: "string_content",
-					In: 12,
-					Fl: 12.12,
-					Bo: true,
+					St:     "string_content",
+					In16:   16,
+					In32:   32,
+					In64:   64,
+					Uint16: 16,
+					Uint32: 32,
+					Uint64: 64,
+					Fl32:   32.32,
+					Fl64:   64.64,
+					Bo:     true,
 				},
 			},
-			want: map[string]string{"bo": "true", "fl": "12.12", "in": "12", "st": "string_content"},
+			want: map[string]string{
+				"bo":     "true",
+				"fl32":   "32.32",
+				"fl64":   "64.64",
+				"in16":   "16",
+				"in32":   "32",
+				"in64":   "64",
+				"st":     "string_content",
+				"uint16": "16",
+				"uint32": "32",
+				"uint64": "64",
+			},
 		},
 	}
 	for _, tt := range tests {
